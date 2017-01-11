b750e1cabc968878a0697b96a3bad0a062c36eeb
Jason Yeo <jasonyeo88@gmail.com>
2017-01-11 12:50:01 +0800

    Add a command that initializes an empty config file

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,54 @@ extern crate yaml_rust;
 extern crate ansi_term;
 
 use ansi_term::Colour::{Red, Cyan};
+use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::path::Path;
 use std::process::Command;
 use std::process::exit;
-use std::io::Read;
+use std::io::prelude::*;
 use yaml_rust::{YamlLoader};
 
+const CONFIG_PATH: &'static str = ".sanity.yml";
+const EMPTY_CONFIG: &'static str =
+"\
+# Define commands and phases to be ran as part of your project's CI/CD workflow. Currently,\n\
+# there are 5 phases: syntax, lint, unit, functional , and security. sanity will run them in\n\
+# that order and fails if any of the commands fails. \n\
+#\n\
+# Ruby project example:\n\
+#\n\
+# syntax:\n\
+#   - rubocop\n\
+#   - reek\n\
+# unit: rspec\n\
+# security: brakeman\n\
+\n\
+syntax:\n\
+lint:\n\
+unit:\n\
+functional:\n\
+security:\n\
+";
+
 fn main() {
-    let path = Path::new(".sanity.yml");
+    let path = Path::new(CONFIG_PATH);
+    let mut args = env::args();
+    args.next();
+    let second = args.next();
+    if second.is_some() {
+        let arg = second.unwrap();
+        match arg.as_str() {
+            "init" => create_sanity_config(),
+            _      => println!("{}", Red.paint(format!("Unknown command {}.", arg))),
+        }
+        exit(0);
+    }
+
 
     let mut file = match File::open(&path) {
-        Err(why) => panic!("Couldn't open {:?} because {}", path, why.description()),
+        Err(why) => panic!("Couldn't open {:?} because {}", &path, why.description()),
         Ok(file) => file,
     };
 
@@ -22,7 +57,7 @@ fn main() {
     file.read_to_string(&mut s);
 
     let docs = match YamlLoader::load_from_str(&s[..]) {
-        Err(why) => panic!("Couldn't parse {:?} because {}", path, why.description()),
+        Err(why) => panic!("Couldn't parse {:?} because {}", &path, why.description()),
         Ok(docs) => docs,
     };
 
@@ -61,6 +96,26 @@ fn main() {
                 println!("{}", Red.paint(msg));
                 exit(1);
             }
+        }
+    }
+}
+
+fn create_sanity_config() {
+    let path = Path::new(CONFIG_PATH);
+    if path.exists() {
+        println!("{}", Red.paint(format!("{:?} already exists.", path)));
+    } else {
+        let mut file = match File::create(&path) {
+            Err(why) => panic!("Couldn't create {:?}: {}", path, why.description()),
+            Ok(file) => file,
+        };
+
+        match file.write_all(EMPTY_CONFIG.as_bytes()) {
+            Err(why) => {
+                panic!("couldn't write to {:?}: {}", path,
+                       why.description())
+            },
+            Ok(_) => println!("{}", Cyan.paint(format!("Created an empty {}", CONFIG_PATH))),
         }
     }
 }
